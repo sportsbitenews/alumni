aa0904be7cbcb4526f0e57460767c10ef4b80a59
Sebastien Saunier <seb@saunier.me>
2015-07-26 13:18:16 +0200

    Add todo with score computation of resource

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -24,4 +24,9 @@ class Resource < ActiveRecord::Base
   acts_as_votable
 
   has_many :answers, as: :answerable
+
+  # TODO(ssaunier): compute on-the-fly score for a resource
+  # score = function(x is days passed since created_at, votes) {
+  #  return votes / (1 + ln(x/2 + 1))
+  # }
 end
