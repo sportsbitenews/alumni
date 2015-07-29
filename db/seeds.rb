65a8f8f8e1942c694a3d4d0885ea3d98bea874f9
Thomas Chretien <thomas@chretien.co>
2015-07-29 14:58:54 +0200

    doc

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
 # USER
+  # using ui_face api + randomuser
 
 users = YAML.load_file('db/support/users.yml')
 users.each do |user|
   random = JSON.load(open('https://randomuser.me/api'))['results'][0]['user']
-  ui_face = JSON.load(open('http://uifaces.com/api/v1/random')) # because randomuser face are ...
+  ui_face = JSON.load(open('http://uifaces.com/api/v1/random'))
 
   user = User.new
   user.gravatar_url = ui_face['image_urls']['epic']
