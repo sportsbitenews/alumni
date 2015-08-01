1dba1634229daed41d49309ca013a448e9c9b370
Sebastien Saunier <seb@saunier.me>
2015-08-01 15:05:13 +0200

    Use Flux pattern with  implementation

--- app/assets/javascripts/components/post_detail_body.jsx
+++ app/assets/javascripts/components/post_detail_body.jsx
@@ -1,7 +1,33 @@
 class PostDetailBody extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      upVotes: props.up_votes
+    };
+
+    // http://facebook.github.io/react/blog/2015/01/27/react-v0.13.0-beta-1.html#autobinding
+    this.onStoreChange = this.onStoreChange.bind(this);
+  }
+
+  componentDidMount() {
+    PostStore.listen(this.onStoreChange);
+  }
+
+  componentWillUnmount() {
+    PostStore.unlisten(this.onStoreChange);
+  }
+
+  onStoreChange(store) {
+    var post = store.getPost(this.props.type, this.props.id);
+    if (post) {
+      this.setState({
+        upVotes: post.up_votes
+      });
+    }
+  }
+
   render() {
-    var upVotes = this.props.up_votes
-    var connectedUsersWhoUpvoted = _.sum(upVotes, (upVote) => upVote.connected_to_slack ? 1 : 0);
+    var connectedUsersWhoUpvoted = _.sum(this.state.upVotes, (upVote) => upVote.connected_to_slack ? 1 : 0);
 
     return (
       <div className='post-detail-body'>
@@ -10,16 +36,16 @@ class PostDetailBody extends React.Component {
         <aside>
           <div className='post-detail-participants'>
             <div className='section-title'>
-              <i className="mdi mdi-account-outline"></i> <span className='section-title-h'>{connectedUsersWhoUpvoted}</span> / {upVotes.length} PARTICIPANTS
+              <i className="mdi mdi-account-outline"></i> <span className='section-title-h'>{connectedUsersWhoUpvoted}</span> / {this.state.upVotes.length} PARTICIPANTS
             </div>
-            {upVotes.map(upVote => {
+            {this.state.upVotes.map(upVote => {
               var participantClasses = classNames({
                 'post-detail-participant': true,
                 'is-offline': !upVote.connected_to_slack
               })
 
               return (
-                <div className={participantClasses}>
+                <div className={participantClasses} key={upVote.id}>
                   <div className='post-detail-participant-avatar'>
                     <img src={upVote.gravatar_url} />
                   </div>
