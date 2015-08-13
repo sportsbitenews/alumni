5fb7c610eb6721b212a5662657b5f2df20c5e6a1
Thomas Chretien <thomas@chretien.co>
2015-08-13 10:56:07 +0200

    answers are now sharable

--- app/assets/javascripts/components/answer_item.jsx
+++ app/assets/javascripts/components/answer_item.jsx
@@ -1,21 +1,65 @@
 class AnswerItem extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      highlighted: false
+    }
   }
 
   render() {
+    answerItemId = classNames({
+      'highlighted': this.state.highlighted
+    })
     return(
-      <div className='answer-item'>
+      <div className="answer-item" id={answerItemId}>
         <div className='answer-avatar'>
           <img src={this.props.user.gravatar_url} className='avatar' />
         </div>
         <div className='answer-body'>
           <div className='answer-header'>
-            {this.props.user.github_nickname}
+            <div className='answer-user-name'>
+              {this.props.user.github_nickname}
+            </div>
+            <div className='answer-time-ago'>
+              {this.props.time_ago} ago
+            </div>
           </div>
           <div className='answer-content' dangerouslySetInnerHTML={{__html: this.props.content}}></div>
+          <div className='answer-item-menu'>
+            <div className='answer-item-share' onClick={this.displaySharingUrl.bind(this)}>
+              SHARE
+            </div>
+          </div>
         </div>
       </div>
     )
   }
+
+  componentDidMount() {
+    var url = window.location.href, idx = url.indexOf("#")
+    var hash = idx != -1 ? url.substring(idx+1) : "";
+
+    if (hash != "") {
+      var id = parseInt(hash.substring(hash.indexOf('-') + 1))
+      if (this.props.id == id) {
+        this.setState({
+          highlighted: true
+        })
+
+        setTimeout(function(){
+          var top = document.getElementById("highlighted").offsetTop; //Getting Y of target element
+          window.scrollTo(0, top);                        //Go there.
+        }, 100)
+      }
+    }
+  }
+
+  displaySharingUrl() {
+    var path = `${this.props.answerable_type.toLowerCase()}_path`;
+    var link = `${window.location.origin}${Routes[path]({ id: this.props.answerable_id })}#answer-${this.props.id}`
+    window.prompt("Copy to clipboard: Ctrl+C / C@md+c, Enter", link);
+  }
+
+
+
 }
--- app/views/answers/_answer.json.jbuilder
+++ app/views/answers/_answer.json.jbuilder
@@ -1,5 +1,8 @@
-json.extract! answer, :id
+json.extract! answer, :id, :answerable_id, :answerable_type
 json.content render_markdown(answer.content)
+json.time_ago time_ago_in_words(answer.created_at)
+
 json.user do
   json.extract! answer.user, :id, :gravatar_url, :github_nickname
 end
+
