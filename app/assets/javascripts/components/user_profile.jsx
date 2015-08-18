e498f34abee479aa38346b7b87b909d6fba02f29
Thomas Chretien <thomas@chretien.co>
2015-08-18 17:47:11 +0200

    first touch

--- app/assets/javascripts/components/user_profile.jsx
+++ app/assets/javascripts/components/user_profile.jsx
@@ -6,14 +6,38 @@ class UserProfile extends React.Component {
     }
 
     return (
-      <div className="container">
-        <h1>{this.props.github_nickname}</h1>
-        Connected: {connected} -
-        <a href={this.props.user_messages_slack_url}>
-          {this.props.user_messages_slack_url}
-        </a>
-
-        <img src={this.props.gravatar_url} className="img-circle" />
+      <div>
+        <div className='user-profile-header'>
+          <div className='container'>
+            <img src={this.props.gravatar_url} className="img-circle user-profile-avatar" />
+            <h1 className='text-center user-profile-username'>
+              {this.props.github_nickname}
+            </h1>
+          </div>
+        </div>
+        <div className='post-submissions-tabs-overlay'>
+          <div className='post-submissions-tabs'>
+            <div className='post-submissions-tab is-active'>
+              <div className='text-center'>{this.props.votes.length}</div>
+              <div className='post-submissions-tab-item'>Upvoted</div>
+            </div>
+            <hr />
+            <div className='post-submissions-tab'>
+              <div className='text-center'>3</div>
+              <div className='post-submissions-tab-item'>Submitted</div>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          {this.props.votes.map( vote => {
+            var props = _.merge(vote, { key: `${vote.type}-${vote.votable_id}` });
+            return React.createElement(eval(vote.type + "ListElement"), props);
+          })}
+          Connected: {connected} -
+          <a href={this.props.user_messages_slack_url}>
+            {this.props.user_messages_slack_url}
+          </a>
+        </div>
       </div>
       )
   }
--- app/views/users/_user.json.jbuilder
+++ app/views/users/_user.json.jbuilder
@@ -2,3 +2,10 @@ json.extract! user, *user_properties
 
 json.connected_to_slack user.connected_to_slack?
 json.user_messages_slack_url user.user_messages_slack_url
+json.votes do
+  json.array! user.votes.each do |vote|
+    post = vote.votable_type.constantize.find(vote.votable_id)
+    json.partial! "posts/post", post: post
+
+  end
+end
