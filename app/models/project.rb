caa412e260b958e812a87155c790ab9dffb64bd0
Thomas Chretien <thomas@chretien.co>
2015-08-20 15:45:42 +0200

    add model project + join table

---
+++ app/models/project.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: projects
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  url        :string
+#  batch_id   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_projects_on_batch_id  (batch_id)
+#
+
+class Project < ActiveRecord::Base
+  belongs_to :batch
+end
