b1dfba19f7086e7a0a07f546d16679948585cdbb
Thomas Chretien <thomas@chretien.co>
2015-08-24 10:40:53 +0200

    uncomment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
-  # before_action :authenticate_user!, unless: :pages_controller?
+  before_action :authenticate_user!, unless: :pages_controller?
 
   include Pundit
   after_action :verify_authorized, except:  [:index], unless: :devise_or_pages_or_admin_controller?
