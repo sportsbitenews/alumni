efa5769779c0a335349853b452f2f32c36c80b06
Thomas Chretien <thomas@chretien.co>
2015-09-08 16:25:58 +0200

    add url to resource

--- app/assets/javascripts/components/resource.jsx
+++ app/assets/javascripts/components/resource.jsx
@@ -1,9 +1,14 @@
 class Resource extends React.Component {
   render() {
     var upVoters = this.props.up_voters;
+    var gradient = "linear-gradient(90deg, rgba(255, 179, 71, 0.9) 10%, rgba(255, 204, 51, 0.9) 90%)"
+    var headerStyle = {
+      background: gradient + ", url('https://wagon-screenshot-as-a-service.herokuapp.com/?url=" + this.props.url + "&width=1600&height=600')",
+      backgroundSize: 'cover'
+    }
     return (
       <div className='post-detail'>
-        <div className='post-detail-header resource-detail'>
+        <div className='post-detail-header resource-detail' style={headerStyle  }>
           <div className='post-detail-header-main'>
             <a href={this.props.url} >
               <div className='post-detail-name'>{this.props.title}</div>
