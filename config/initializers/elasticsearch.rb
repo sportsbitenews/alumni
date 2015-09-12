d3c6fd56d5cd6eaaf634b25fd5b87ee61caa7a19
Sebastien Saunier <seb@saunier.me>
2015-09-12 10:16:49 +0200

    Search box ssl url in production

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -2,7 +2,7 @@ require "faraday"
 require "typhoeus/adapters/faraday"
 Ethon.logger = Logger.new("/dev/null")
 
-host = ENV.fetch('SEARCHBOX_URL', '127.0.0.1:9200')
+host = ENV.fetch('SEARCHBOX_SSL_URL', '127.0.0.1:9200')
 
 Searchkick.client = Elasticsearch::Client.new(
   hosts: [host], retry_on_failure: true)
