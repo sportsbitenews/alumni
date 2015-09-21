10a1ddc9ff5a70041e98a32fddba7678d291c2ca
Sebastien Saunier <seb@saunier.me>
2015-09-21 11:38:05 +0200

    Invite to slack

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -5,7 +5,7 @@ require "omniauth-slack"
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
   config.omniauth :github, ENV['GITHUB_APP_ID'], ENV['GITHUB_APP_SECRET'], scope: 'user:email'
-  config.omniauth :slack, ENV['SLACK_CLIENT_ID'], ENV['SLACK_CLIENT_SECRET'], scope: 'identify', team: ENV['SLACK_ALUMNI_TEAM_ID']
+  config.omniauth :slack, ENV['SLACK_CLIENT_ID'], ENV['SLACK_CLIENT_SECRET'], scope: 'admin', team: ENV['SLACK_ALUMNI_TEAM_ID']
 
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
