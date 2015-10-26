a0d7467ed5b71d8c90c7be1b7a690d3eb01a1639
Boris Paillard <boris.paillard@gmail.com>
2015-10-26 16:57:44 +0100

    add specifics markdown

--- app/models/city.rb
+++ app/models/city.rb
@@ -28,6 +28,7 @@
 #  classroom_picture_file_size    :integer
 #  classroom_picture_updated_at   :datetime
 #  course_locale                  :string
+#  specifics                      :text
 #
 # Indexes
 #
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151026142240) do
+ActiveRecord::Schema.define(version: 20151026154003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(version: 20151026142240) do
     t.integer  "classroom_picture_file_size"
     t.datetime "classroom_picture_updated_at"
     t.string   "course_locale"
+    t.text     "specifics"
   end
 
   add_index "cities", ["slug"], name: "index_cities_on_slug", unique: true, using: :btree
