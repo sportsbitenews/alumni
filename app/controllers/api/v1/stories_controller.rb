02f8948af160d44a8578f67330f3ccf7a1431eff
Sebastien Saunier <seb@saunier.me>
2015-11-04 16:50:39 +0100

    Emergency fix

--- app/controllers/api/v1/stories_controller.rb
+++ app/controllers/api/v1/stories_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::StoriesController < Api::V1::BaseController
   def index
-    excluded_ids = params[:excluded_ids].split(',')
-    limit = params[:limit].to_i
+    excluded_ids = (params[:excluded_ids] || "").split(',')
+    limit = (params[:limit] || "").to_i
 
     @stories = Story.includes(:company).where(published: true).where.not(id: excluded_ids).limit(limit).order("RANDOM()")
     if @stories.size < limit
