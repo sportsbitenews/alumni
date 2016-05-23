ab5d59c698ce8a53e6e34897b8b677d1a1e25c2a
kevin robert <kevin.j.robert@gmail.com>
2016-05-23 15:18:55 +0200

    run migration on local master branch

--- db/schema.rb
+++ db/schema.rb
@@ -293,6 +293,12 @@ ActiveRecord::Schema.define(version: 20160507180124) do
     t.string   "twitter_nickname"
     t.boolean  "noindex",                default: false, null: false
     t.text     "private_bio"
+    t.string   "status"
+    t.text     "mood"
+    t.string   "linkedin_nickname"
+    t.string   "facebook_nickname"
+    t.jsonb    "pre_wagon_experiences",                               array: true
+    t.jsonb    "post_wagon_experiences",                              array: true
   end
 
   add_index "users", ["batch_id"], name: "index_users_on_batch_id", using: :btree
