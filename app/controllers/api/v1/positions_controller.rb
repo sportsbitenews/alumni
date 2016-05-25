ad9037263b7926a9f27f00e63b543cb21bd6eb0f
Boris Paillard <boris.paillard@gmail.com>
2016-05-25 18:10:10 +0200

    add early loading of positions associations

--- app/controllers/api/v1/positions_controller.rb
+++ app/controllers/api/v1/positions_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::PositionsController < Api::V1::BaseController
   def index
-    @positions = Position.includes(:user, :company).all
+    @positions = Position.includes({user: :batch}, :company).all
   end
 end
