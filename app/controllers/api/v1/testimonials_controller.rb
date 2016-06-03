926c14022610238e40d8c8c76860a14535f4c878
Boris Paillard <boris.paillard@gmail.com>
2016-06-03 12:19:13 +0200

    add city parameter to filter testimonials

--- app/controllers/api/v1/testimonials_controller.rb
+++ app/controllers/api/v1/testimonials_controller.rb
@@ -1,5 +1,8 @@
 class Api::V1::TestimonialsController < Api::V1::BaseController
   def index
     @testimonials = Testimonial.where locale: params[:locale]
+    if params[:city]
+      @testimonials.to_a.select!{ |testimonial| testimonial.user.batch.city.name.downcase == params[:city] }
+    end
   end
 end
