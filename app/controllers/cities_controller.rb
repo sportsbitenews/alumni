13e205db4e3472165eb57406fb3c70204db4336b
Sebastien Saunier <seb@saunier.me>
2016-06-16 08:56:45 +0200

    Ensure an ordered list is create for each city when editing Staff/TA list

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -16,8 +16,8 @@ class CitiesController < ApplicationController
   end
 
   def edit
-    @teacher_ordered_list = OrderedList.find_by_name "#{params[:id]}_teachers"
-    @teaching_assistant_ordered_list = OrderedList.find_by_name "#{params[:id]}_teacher_assistants"
+    @teacher_ordered_list = OrderedList.find_or_create_by!(name: "#{params[:id]}_teachers", element_type: 'User')
+    @teaching_assistant_ordered_list = OrderedList.find_or_create_by!(name: "#{params[:id]}_teacher_assistants", element_type: 'User')
     @teachers = User.where(github_nickname: @teacher_ordered_list.slugs).sort_by {|t| @teacher_ordered_list.slugs.index(t.github_nickname) }
     @teaching_assistants = User.where(github_nickname: @teaching_assistant_ordered_list.slugs).sort_by {|t| @teaching_assistant_ordered_list.slugs.index(t.github_nickname) }
   end
