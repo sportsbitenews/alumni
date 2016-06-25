4ec09be3130c768105814e2cf5ba1525c299ff55
Sebastien Saunier <seb@saunier.me>
2016-06-25 12:14:21 +0100

    Add phone number on city

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160525141433) do
+ActiveRecord::Schema.define(version: 20160625102116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,8 +68,8 @@ ActiveRecord::Schema.define(version: 20160525141433) do
 
   create_table "cities", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at",                       null: false
-    t.datetime "updated_at",                       null: false
+    t.datetime "created_at",                                       null: false
+    t.datetime "updated_at",                                       null: false
     t.string   "location"
     t.string   "address"
     t.text     "description_fr"
@@ -103,6 +103,9 @@ ActiveRecord::Schema.define(version: 20160525141433) do
     t.string   "mailchimp_api_key"
     t.string   "slack_channel_id"
     t.text     "marketing_specifics"
+    t.string   "contact_phone_number"
+    t.boolean  "contact_phone_number_displayed",   default: false, null: false
+    t.string   "contact_phone_number_name"
   end
 
   add_index "cities", ["slug"], name: "index_cities_on_slug", unique: true, using: :btree
