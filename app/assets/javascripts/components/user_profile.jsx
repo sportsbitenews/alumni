55ff2f9a38cb63899569338148cc03e41fbd1230
kevin robert <kevin.j.robert@gmail.com>
2016-11-30 13:38:20 +0100

    wip

--- app/assets/javascripts/components/user_profile.jsx
+++ app/assets/javascripts/components/user_profile.jsx
@@ -73,7 +73,6 @@ class UserProfile extends React.Component {
         <div className='user-profile-header'>
           <div className='container'>
             <img src={this.props.thumbnail} className="img-circle user-profile-avatar" />
-
             <h1 className='text-center user-profile-name'>
               {`${this.props.first_name} ${this.props.last_name}`} <span className={badgeConnectedClasses} />
             </h1>
--- app/views/users/show.json.jbuilder
+++ app/views/users/show.json.jbuilder
@@ -1,4 +1,5 @@
 json.partial! 'user', user: @user
 json.current_user do
   json.user_signed_in user_signed_in?
+  json.github_nickname current_user.github_nickname
 end
