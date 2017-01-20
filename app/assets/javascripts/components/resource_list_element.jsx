797d1052570c0ca6bec65bc8220ecdfe7c09f51b
Sebastien Saunier <seb@saunier.me>
2017-01-20 11:12:17 +0100

    Fix infinite scroll / search

--- app/assets/javascripts/components/resource_list_element.jsx
+++ app/assets/javascripts/components/resource_list_element.jsx
@@ -8,17 +8,14 @@ class ResourceListElement extends PostListElement {
     var thumbnailStyle = {
       backgroundImage: "url('"+ this.props.screenshot_url +"');"
     }
-    var tooltip = (<ReactBootstrap.Tooltip className='tooltip-resource'>{this.props.tagline}</ReactBootstrap.Tooltip>)
 
     return (
         <div className='post-item resource-item' ref='listItem'>
           <div className='flex resource-item-meta'>
             <div className='post-item-text'>
-              <ReactBootstrap.OverlayTrigger placement='bottom' trigger={['hover', 'focus']} delayShow={200} overlay={tooltip}>
-                <div className='post-item-text-name'>
-                  {this.props.title}
-                </div>
-              </ReactBootstrap.OverlayTrigger>
+              <div className='post-item-text-name'>
+                {this.props.title}
+              </div>
 
               <div className='post-item-text-tagline'>
                 posted by
--- app/assets/javascripts/components/student_item.jsx
+++ app/assets/javascripts/components/student_item.jsx
@@ -14,37 +14,30 @@ class StudentItem extends React.Component {
       'is-removed': this.state.removed
     })
 
-    var tooltip = (<ReactBootstrap.Tooltip className='tooltip-resource'>Invite to Kitt & Slack</ReactBootstrap.Tooltip>)
-
     return(
-      <div>
-          <div className={itemClasses}>
+      <div className={itemClasses}>
 
-            <div className='student-item-actions'>
-              <a onClick={this.handleDelete.bind(this)}>
-                <i className='mdi mdi-close'></i>
-              </a>
-            </div>
-            <div className="student-item-infos">
-              <div className="student-item-name">
-                {this.props.student.first_name} {this.props.student.last_name}
-              </div>
-              <div className="student-item-username">
-                @{this.props.student.github_nickname}
-              </div>
-            </div>
-            <div className="student-item-avatar">
-              <img src={this.props.student.thumbnail} />
-            </div>
-            <div className='student-item-cta' onClick={this.handleValidation.bind(this)}>
-              <ReactBootstrap.OverlayTrigger placement='top' trigger={['hover', 'focus']} delayShow={30} overlay={tooltip}>
-                <a>
-                  <i className='mdi mdi-check'></i>
-                </a>
-              </ReactBootstrap.OverlayTrigger>
-            </div>
+        <div className='student-item-actions'>
+          <a onClick={this.handleDelete.bind(this)}>
+            <i className='mdi mdi-close'></i>
+          </a>
+        </div>
+        <div className="student-item-infos">
+          <div className="student-item-name">
+            {this.props.student.first_name} {this.props.student.last_name}
           </div>
-
+          <div className="student-item-username">
+            @{this.props.student.github_nickname}
+          </div>
+        </div>
+        <div className="student-item-avatar">
+          <img src={this.props.student.thumbnail} />
+        </div>
+        <div className='student-item-cta' onClick={this.handleValidation.bind(this)}>
+          <a>
+            <i className='mdi mdi-check'></i>
+          </a>
+        </div>
       </div>
     )
   }
